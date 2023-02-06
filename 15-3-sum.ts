f5e91ccc947209efa961552f83e4e4caa993926f
Bobby Mathews <bnaoki12@gmail.com>
2023-02-06 13:21:51 -0800

    modified:   15-3-sum.ts

--- 15-3-sum.ts
+++ 15-3-sum.ts
@@ -1,20 +1,10 @@
-function threeSum(nums: number[]): number[][] {
+function threeSum(nums: number[]): Array<[number, number, number]> {
   const len = nums.length
   if (len < 3) {
     return []
   }
 
-  // const sorted = [...nums].sort((a, b) => {
-  //   if (a < b) {
-  //     return -1
-  //   } else if (a > b) {
-  //     return 1
-  //   } else {
-  //     return 0
-  //   }
-  // })
-
-  let validSets: Array<[number, number, number]> = []
+  const validSets: Array<[number, number, number]> = []
 
   // Sort the input array. Assume this is allowable.
   nums.sort((a, b) => {
@@ -35,9 +25,9 @@ function threeSum(nums: number[]): number[][] {
       continue
     }
 
-    // A 0 or positive number cannot sum to 0 with 2 other postiive numbers. So we can
-    // stop checking we reach this point.
-    if (target >= 0) {
+    // A positive number cannot sum to 0 with 2 other positive numbers wo we can
+    // stop checking when we reach this point.
+    if (target > 0) {
       break
     }
 
@@ -46,28 +36,35 @@ function threeSum(nums: number[]): number[][] {
 
     while (j < k) {
       // Skip duplicates on the left side.
-      if (j > 0 && nums[j] === nums[j - 1]) {
-        j += 1
-        continue
-      }
+      // if (j > 0 && nums[j] === nums[j - 1]) {
+      //   j += 1
+      //   continue
+      // }
 
-      // Skip duplicates on the right side.
-      if (k < len - 1 && nums[k] === nums[k + 1]) {
-        k -= 1
-        continue
-      }
+      // // Skip duplicates on the right side.
+      // if (k < len - 1 && nums[k] === nums[k + 1]) {
+      //   k -= 1
+      //   continue
+      // }
 
       const sum = target + nums[j] + nums[k]
 
-      if (sum === 0) {
-        validSets.push([target, nums[j], nums[k]])
-      } else if (sum < 0) {
+      if (sum < 0) {
         j += 1
-      } else {
+      } else if (sum > 0) {
         k -= 1
+      } else {
+        validSets.push([target, nums[j], nums[k]])
+        j += 1
+        k -= 1
+        while (j < k && nums[j] === nums[j - 1]) {
+          j += 1
+        }
       }
     }
   }
 
   return validSets
 }
+
+console.log(threeSum([-1, 0, 1, 2, -1, -4]))
